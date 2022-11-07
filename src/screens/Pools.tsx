1a800a5741be0b457fef5ff3b26e86d70dd0ba60
Herbert Veloso <herbert.oliveira.veloso@gmail.com>
2022-11-06 22:56:41 -0300

    Create polls list

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,38 @@
-import { VStack, Icon } from "native-base";
+import { useCallback, useState } from "react";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { VStack, Icon, FlatList } from "native-base";
 import { Octicons } from "@expo/vector-icons";
 
+import { api } from "../services/api";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
-import { useNavigation } from "@react-navigation/native";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolPros } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [polls, setPolls] = useState<PoolPros[]>([]);
+
   const { navigate } = useNavigation();
+
+  async function fetchPolls() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPolls(response.data.polls);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPolls();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -27,6 +53,18 @@ export function Pools() {
             onPress={() => navigate("find")}
           />
         </VStack>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <FlatList
+            data={polls}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <PoolCard data={item} />}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            ListEmptyComponent={EmptyPoolList}
+          />
+        )}
       </VStack>
     </VStack>
   );
